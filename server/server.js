31edd88fbc957ac77dcbfbb9e24992a14e7f203c
Andy de Torres <detorres@stevens>
2017-08-26 09:59:30 -0700

    add GET /todos

--- server/server.js
+++ server/server.js
@@ -11,19 +11,18 @@ app.use(bodyParser.json());
 
 app.post('/todos', (req, res) =>
 {
-  var todo = new Todo({
-    text: req.body.text
-  });
+  var todo = new Todo({ text: req.body.text });
 
   todo.save()
-    .then((doc) =>
-    {
-      res.send(doc);
-    },
-    (err) =>
-    {
-      res.status(400).send(err);
-    });
+    .then((doc) => res.send(doc),
+          (err) => res.status(400).send(err));
+});
+
+app.get('/todos', (req, res) =>
+{
+  Todo.find()
+    .then((todos) => res.send({todos}),
+          (err) => res.status(400).send(err));
 });
 
 app.listen(3000, () =>
