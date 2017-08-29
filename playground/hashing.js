773838119925ab2edbdc7d6dae24ed80943cc6a2
Andy de Torres <detorres@stevens>
2017-08-29 07:00:07 -0700

    add password hashing

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,38 +1,24 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
 var data = {
   id: 10
 };
 
-var token = jwt.sign(data, '123abc');
-console.log('Token:', token);
+var password = 'abc123!';
 
-var decoded = jwt.verify(token, '123abc');
-console.log('decoded', decoded);
+// bcrypt.genSalt(10, (err, salt) =>
+// {
+//   bcrypt.hash(password, salt, (err, hash) =>
+//   {
+//     console.log(hash);
+//   });
+// });
 
-// var message = 'I am user number 3.';
-// var hash = SHA256(message).toString();
-//
-// console.log("Message", message);
-// console.log("Hash", hash);
-//
-// var data = {
-//   id: 4
-// };
-//
-// var token = {
-//   data,
-//   hash: SHA256(JSON.stringify(data) + 'somesecret').toString()
-// };
-//
-// var resHash= SHA256(JSON.stringify(token.data) + 'somesecret').toString();
-//
-// if (resHash === token.hash)
-// {
-//   console.log('data was not changed');
-// }
-// else
-// {
-//   console.log('data was changed');
-// }
+var hashedPassword = '$2a$10$mn.TS4H11jdMJcjFexZg8O.cPUvIsRIfVeLBaJTQeGCpq.VrEwKTW';
+
+bcrypt.compare(password, hashedPassword, (err, res) =>
+{
+  console.log(res);
+});
